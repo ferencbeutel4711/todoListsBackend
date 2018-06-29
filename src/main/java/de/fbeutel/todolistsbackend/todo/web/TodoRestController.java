f102502494f38d51a2204aff8cad795dca7dabbb
ferencbeutel <ferenc1993beutel@gmx.de>
2018-06-29 16:10:13 +0200

    Add getAll and deleteAll endpoints

--- src/main/java/de/fbeutel/todolistsbackend/todo/web/TodoRestController.java
+++ src/main/java/de/fbeutel/todolistsbackend/todo/web/TodoRestController.java
@@ -7,6 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/todos")
@@ -16,6 +20,14 @@ public class TodoRestController {
 
     public TodoRestController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAll() {
+        return ResponseEntity.ok()
+                .body(StreamSupport.stream(todoRepository.findAll().spliterator(), false)
+                        .map(Todo::getSerializedTodo)
+                        .collect(toList()));
     }
 
     @GetMapping("/{todoId}")
@@ -33,10 +45,17 @@ public class TodoRestController {
         return ResponseEntity.created(URI.create("http://127.0.0.1:9999/todos/" + updatedId)).build();
     }
 
+    @DeleteMapping
+    public ResponseEntity deleteAll() {
+        todoRepository.deleteAll();
+
+        return ResponseEntity.ok().build();
+    }
+
     @DeleteMapping("/{todoId}")
     public ResponseEntity deleteTodo(@PathVariable final String todoId) {
         todoRepository.deleteById(todoId);
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 }
